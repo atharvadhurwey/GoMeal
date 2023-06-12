a0d7022e6da9c6bc273ece76e05a07174b26eb73
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-06-12 12:54:45 +0530

    sanity client fix

--- gomeal/utils/client.js
+++ gomeal/utils/client.js
@@ -2,6 +2,7 @@ import sanityClient from '@sanity/client';
 import config from './config';
 const client = sanityClient({
   projectId: config.projectId,
+  apiVersion: '2022-09-27',
   dataset: config.dataset,
   useCdn: true,
 });
